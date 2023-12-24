02f05720cc13d10cda4dfc954504c3a72f4683f2
Moustafa <moustafaadel097@gmail.com>
2023-12-24 05:49:26 +0200

    initial commit

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ const cors = require("cors");
 const dotenv = require("dotenv").config();
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: ["http://localhost:3000","https://nooneshop.onrender.com"]
+}));
 app.use(express.json({ limit: "10mb" }));
 
 const PORT = process.env.PORT || 8000;
